bcbe548322a518088dc0cff7a59cf6cbdbe24f3b
yuu278 <diveing@icloud.com>
2025-03-30 15:05:20 +0900

    frontendフォルダの実装

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // すべてのJS、JSX、TS、TSXファイルをスキャン
+  ],
+  future: {
+  },
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "aqua", "pastel"],
+  },
+};
