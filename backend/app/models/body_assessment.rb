147c65b99146857ccc701fa9d0108b1e651a7831
yuu278 <diveing@icloud.com>
2025-11-24 21:53:08 +0900

    update: 不要になった機能を削除し、必須のバリデーションを追加

--- backend/app/models/body_assessment.rb
+++ backend/app/models/body_assessment.rb
@@ -1,23 +1,11 @@
 class BodyAssessment < ApplicationRecord
   belongs_to :user
 
-  validates :assessment_date, presence: true
-  validates :shoulder_score, :neck_score, :back_score, :hip_score, :knee_score, :ankle_score,
-            presence: true,
-            numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
-
-  def total_score
-    shoulder_score + neck_score + back_score + hip_score + knee_score + ankle_score
-  end
-
-  def problem_areas
-    areas = []
-    areas << "shoulder" if shoulder_score > 5
-    areas << "neck" if neck_score > 5
-    areas << "back" if back_score > 5
-    areas << "hip" if hip_score > 5
-    areas << "knee" if knee_score > 5
-    areas << "ankle" if ankle_score > 5
-    areas
-  end
+  # 必須項目のバリデーション
+  validates :pain_area, presence: true
+  validates :pain_types, presence: true
+  validates :duration, presence: true
+  validates :job_types, presence: true
+  validates :exercise_habits, presence: true
+  validates :posture_habits, presence: true
 end
