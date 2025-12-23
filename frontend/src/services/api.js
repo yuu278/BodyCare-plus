c10ac03e7e3f6c1bdb79c775f543239ffd14bd50
yuu278 <diveing@icloud.com>
2025-12-23 20:45:06 +0900

    fix: API_URLの修正

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/api/v1';
+const API_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3001/api/v1';
 
 const apiClient = axios.create({
   baseURL: API_URL,
